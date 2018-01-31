0ef213164494a97d7bf9ffa11ce1177d1f6f3983
Panneerselvam <panneerselvam.nithiyanantham@merrillcorp.com>
2018-01-31 19:57:13 +0530

    TRAIL-002 code change in Interface

--- src/main/java/com/oauthex/common/swagger/web/EnableCommonSwagger.java
+++ src/main/java/com/oauthex/common/swagger/web/EnableCommonSwagger.java
@@ -1,10 +1,19 @@
 package com.oauthex.common.swagger.web;
 
-import com.oauthex.common.swagger.config.CommonSwaggerProperties;
-import org.springframework.context.annotation.Configuration;
+import com.oauthex.common.swagger.config.SwaggerConfig;
 import org.springframework.context.annotation.Import;
 
-@Configuration
-@Import(CommonSwaggerProperties.class)
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(SwaggerConfig.class)
 public @interface EnableCommonSwagger {
 }
